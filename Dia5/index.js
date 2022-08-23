5f980ea1b5586a45ff57a33e2c6d7f1613b40a99
Juan Alberto <glezjuan97@gmail.com>
2022-08-22 19:43:44 -0700

    update1

--- Dia5/index.js
+++ Dia5/index.js
@@ -1,9 +1,25 @@
 const formulario = document.getElementById('formulario')
 
+const dbUser = 'juan'
+const dbPassword = '1234abc'
+
+function validar(u,p){
+    if(u === '' || p === ''){
+        console.log('Por favor ingresa un dato');
+    }
+    else{
+        if(u === dbUser && p === dbPassword){
+            console.log('Bienvenido a tu cuenta');
+        }
+        else{
+            console.log('Usuario o password incorrecto');
+        }
+    }
+}
+
 formulario.addEventListener('submit', function(evento){
     evento.preventDefault()
-    let usuario = document.getElementById('usuario')
-    let password = document.getElementById('password')
-    console.log(usuario)
-    console.log(password);
+    let usuario = document.getElementById('usuario').value
+    let password = document.getElementById('password').value
+    validar(usuario, password)
 })
